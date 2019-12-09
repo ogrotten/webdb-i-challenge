cc6bb3ae9296932544a59007cd16b57898e3402d
Darrin Lowery <lambda@monsto.com>
2019-12-09 15:48:21 -0600

    before "yarn" (forgot to install)

---
+++ routes/dbroutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const db = require('./data/dbConfig.js');
+const router = express.Router();
+
+const knex= require("../data/dbConfig.js")
+
+router.get("/", (req, res) => {
+	knex
+	.select("*")
+	.from("Accounts")
+	.then(accts => {
+		res.status(200).json(accts)
+	})
+	.catch(err => {
+		res.status(500).json({msg: "Problem", err: err});
+	});
+});
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
+const dbrouter = require("./routes/dbroutes.js")
+
 const db = require('./data/dbConfig.js');
 
 const server = express();
+server.use("/api/db", dbrouter)
 
 server.use(express.json());
 
